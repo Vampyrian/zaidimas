63cffe2e7d758ddfe4bd2e94143d585b8c5843a6
Vampyrian <Zilvinasees@gmail.com>
2018-03-11 15:11:55 +0200

    Bandau duoti uzklausa bitrex

--- app.js
+++ app.js
@@ -15,3 +15,22 @@ const server = http.createServer((req, res) => {
 
 server.listen(port);
 console.log('serveris pasileido ant porto: ' + port);
+
+// https://bittrex.com/api/v1.1/public/getticker?market=BTC-LTC
+
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://bittrex.com/api/v1.1/public/getticker?market=BTC-LTC', true);
+
+xhr.send();
+
+if (xhr.status != 200) {
+    alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+} else {
+    alert( xhr.responseText ); // responseText -- текст ответа.
+}
+
+
+
+
+
